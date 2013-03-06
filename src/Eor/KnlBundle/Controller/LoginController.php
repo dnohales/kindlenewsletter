a676854f370393bb9c4159b6ef1cca0122927ad1
Damián Nohales <damiannohales@gmail.com>
2013-03-06 09:44:28 -0300

    Smarter redirection to homepage when user request to login routes

--- src/Eor/KnlBundle/Controller/LoginController.php
+++ src/Eor/KnlBundle/Controller/LoginController.php
@@ -6,7 +6,6 @@ use Eor\KnlBundle\GoogleReader\AccessDeniedException;
 use Eor\KnlBundle\Security\GoogleAccessToken;
 use Eor\KnlBundle\Entity\User;
 use Symfony\Component\Validator\Constraints\Url;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * Description of LoginController
@@ -17,15 +16,37 @@ class LoginController extends Controller {
 
 	public function loginAction()
 	{
-		$this->checkLogin();
+		//I get the token in this ugly way until I discover how to get the token
+		//from I firewall in which I am not.
+		if($this->getRequest()->getSession()->has('_security_secured_area')){
+			return $this->redirect($this->generateUrl('homepage'));
+		}
+		
 		return $this->render('EorKnlBundle:Login:login.html.twig', array(
 			'loginUrl' => $this->get('greader_client')->getAuthenticator()->getLoginUrl()
 		));
 	}
 	
+	/**
+	 * Analyze and authorize the Google OAuth2 code and initialize the user
+	 * session.
+	 * @return Response
+	 * @throws \Exception
+	 * @todo put this logic into an AuthenticationManager/Listener or whatever
+	 * needed
+	 */
 	public function googleCheckAction()
 	{
-		$this->checkLogin();
+		//If we have access token and is not expired, redirect to homepage.
+		//This is because if we redirect with expired token, the homepage will
+		//redirect to Google OAuth2 URL again and will cause recursion.
+		if($this->getRequest()->getSession()->has('_security_secured_area')){
+			/* @var $token GoogleAccessToken */
+			$token = unserialize($this->getRequest()->getSession()->get('_security_secured_area'));
+			if(!$token->getTokenInformation()->isAccessTokenExpired()){
+				return $this->redirect($this->generateUrl('homepage'));
+			}
+		}
 		
 		/* @var $authenticator \Eor\KnlBundle\GoogleReader\Authenticator */
 		$authenticator = $this->get('greader_client')->getAuthenticator();
@@ -77,15 +98,6 @@ class LoginController extends Controller {
 			$this->get('session')->setFlash('login.error', 'An unexpected error occurred during logon.');
 			$this->get('logger')->err('Login error: '.$e->getMessage());
 			return $this->redirect($this->generateUrl('login'));
-		}
-	}
-	
-	public function checkLogin()
-	{
-		if($this->getRequest()->getSession()->has('_security_secured_area')){
-			throw new HttpException(302, null, null, array(
-				'Location' => $this->generateUrl('homepage')
-			));
 		}
 	}
 
